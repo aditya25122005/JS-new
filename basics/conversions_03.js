e2d676e2dd59035c9b4b6f57cfbd587b86064216
aditya25122005 <aditya25pda@gmail.com>
2025-01-09 19:40:48 +0530

    commit

---
+++ basics/conversions_03.js
@@ -0,0 +1,25 @@
+// let score=true;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let value=Number(score);
+// console.log(value);
+// console.log(typeof value);
+
+//"33" => 33
+// "33abc" => NaN  type is number
+// true => 1; false => 0
+
+let isloggedIn=1;
+
+let booleanIsLoggedIn=Boolean(isloggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1=> true
+// 0 => false
+// "" => false
+// null => false
+// undefined=>false
+// "aditya" =>true
+console.log(typeof booleanIsLoggedIn)
